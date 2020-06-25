14bfdb909aad65129b208ef80b80d55f88870437
pudu87 <sam.cappelle87@gmail.com>
2020-06-25 17:56:57 -0400

    add delete comments option

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,4 +12,12 @@ class CommentsController < ApplicationController
     redirect_to article_path(@comment.article)
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @article = Article.find("#{@comment.article_id}")
+    flash.notice = "Comment Deleted!"
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
 end
